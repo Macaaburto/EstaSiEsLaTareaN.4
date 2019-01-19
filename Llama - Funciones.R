9ab68049cbaa90a03d84aab22c125a1155c039bd
Macaaburto <macarena.aburtoc@utem.cl>
2019-01-18 22:34:54 -0400

    Funciones para desarrollar ejercicio de la llama

---
+++ Llama - Funciones.R
@@ -0,0 +1,10 @@
+source ('Llama funciones y ejercicio.R')
+
+# Funciones y Ejercicio: Llama
+
+#Contar Llama
+a<-" Porque la llama que llama estando en llamas me llama, alguien mas llama"
+b<-" "
+c<-"Porque la llama que llama estando en llamas me llama , alguien mas llama"
+Contar<-strsplit(a,b)[[1]]
+Contar2<-strsplit(c,b)[[1]]
